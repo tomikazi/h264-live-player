18464f14e2dbca0b77d7b5373eccd45f1adde06d
Thomas Vachuska <tom@opennetworking.org>
2021-01-06 16:00:31 -0800

    Fixed to allow multiple cameras with URL rewriting

--- server.js
+++ server.js
@@ -2,23 +2,25 @@
 
 const http = require('http');
 const express = require('express');
+const args = require('minimist')(process.argv.slice(2))
 
 const WebStreamerServer = require('./lib/raspivid');
 
 const app = express();
+const url = args['url'] || '/camera'
 
 //public website
 app.use(express.static(__dirname + '/public'));
-app.use('/camera', express.static(__dirname + '/vendor/dist'));
+app.use(url, express.static(__dirname + '/vendor/dist'));
 
 const server = http.createServer(app);
 const streamer = new WebStreamerServer(server, {});
 
-console.log("Starting server...");
+console.log(`Starting server on ${url}...`);
 server.listen(5000);
 
 process.on('SIGINT', function () {
-    console.log("Caught interrupt signal");
+    console.log('Caught interrupt signal');
     streamer.stop();
     process.exit();
 });
